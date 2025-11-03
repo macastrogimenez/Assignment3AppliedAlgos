d48ea19f3cbcb8f9b2f2b65c5c73c182dd0d0774
macastrogimenez <migca@itu.dk>
2025-11-03 18:30:42 +0100

    Add initial VSCode settings and update BidirectionalDijkstra class structure

--- contraction-hierarchies-template/app/src/main/java/ch/BidirectionalDijkstra.java
+++ contraction-hierarchies-template/app/src/main/java/ch/BidirectionalDijkstra.java
@@ -1,6 +1,37 @@
 package ch;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+import org.checkerframework.checker.units.qual.s;
+
+// INPUT
+// first line: n = number of vertices , m = number of edges
+// n lines (nodes): int = number of the vertex, float = longitude, float = latitude 
+// m lines (edges): int = start (vertex), int = end (vertex), int = cost
+
 public class BidirectionalDijkstra {
 
-    // To be made!
+    Set<Integer> settled = new HashSet<>();
+    Map<Integer, Float> dijkstraLeft = new HashMap<>();
+    Map<Integer, Float> dijkstraRight = new HashMap<>();
+    int d = Integer.MAX_VALUE;
+
+    //TODO: check that this is correctly initiated to the right values of s and t
+    public BidirectionalDijkstra(){ 
+        dijkstraLeft.put(0, 0.0f);
+        dijkstraRight.put(0, 0.0f);
+        
+        PriorityQueue<PQElem> queueLeft = new PriorityQueue<>();
+        PriorityQueue<PQElem> queueRight = new PriorityQueue<>();
+
+
+
+    }
+
+
+
 }
